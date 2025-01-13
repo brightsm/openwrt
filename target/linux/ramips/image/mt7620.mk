3d872342c3c6542912068c631ef4127c82ba92fc
brightsm <brightsm@163.com>
2025-01-13 20:28:29 +0800

    Update mt7620.mk
    Signed-off-by: brightsm <brightsm@163.com>

--- target/linux/ramips/image/mt7620.mk
+++ target/linux/ramips/image/mt7620.mk
@@ -418,6 +418,14 @@ define Device/zbt-we826
 endef
 TARGET_DEVICES += zbt-we826
 
+define Device/HJ5205-F
+  DTS := HJ5205-F
+  IMAGE_SIZE := $(ralink_default_fw_size_16M)
+  DEVICE_TITLE := HuiJieTek HJ5205-F
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ohci kmod-mt76 kmod-sdhci-mt7620 
+endef
+TARGET_DEVICES += HJ5205-F
+
 define Device/zbt-wr8305rt
   DTS := ZBT-WR8305RT
   DEVICE_TITLE := Zbtlink ZBT-WR8305RT
